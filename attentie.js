dacc805d57e63b42b63d1ceb4aa3614229ad4875
crafterkb <96026601+crafterkb@users.noreply.github.com>
2021-12-25 17:32:28 +0100

    Add files via upload

---
+++ attentie.js
@@ -0,0 +1,16 @@
+window.onload = attentie();
+
+function attentie() {
+let img = document.querySelector(".attentie");
+img.style.marginTop = "0px";
+
+setTimeout(function() {img.style.marginTop = "-200px"}, 8000);
+}
+
+
+
+
+
+
+
+
