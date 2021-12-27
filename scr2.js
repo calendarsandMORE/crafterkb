f5f1d350a87e09e41aa1e85b3db2a860260b002a
crafterkb <96026601+crafterkb@users.noreply.github.com>
2021-12-27 15:33:23 +0100

    Update scr2.js

--- scr2.js
+++ scr2.js
@@ -1,4 +1,9 @@
-window.onload = laadmenu;
+window.onload = doEverything;
+
+function doEverything() {
+laadmenu();
+laadfooter();
+}
 
 let menuitems = [
 ["Home", "index"],
@@ -22,4 +27,25 @@ menu.appendChild(aa);
 
 }
 
+function laadfooter() {
+let body = document.querySelector("body");
+let footer = document.createElement("div");
+footer.setAttribute("class", "footer");
+
+let date = new Date();
+let year = date.getFullYear();
+
+let para = document.createElement("p");
+para.innerHTML = "&copy; " + year + " Crafter Kingston. All rights reserved.";
+footer.appendChild(para);
+
+let a = document.createElement("a");
+a.setAttribute("href", "index.htm");
+let img = document.createElement("img");
+img.setAttribute("src", "ytlogo.jpg");
+a.appendChild(img);
+footer.appendChild(a);
+
+body.appendChild(footer);
+}
 
