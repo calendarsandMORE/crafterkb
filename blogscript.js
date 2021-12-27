c8906cbc99db74f21b6fc08baa92f124cb156c8f
crafterkb <96026601+crafterkb@users.noreply.github.com>
2021-12-27 11:16:00 +0100

    Update blogscript.js

--- blogscript.js
+++ blogscript.js
@@ -3,13 +3,14 @@ Syntax:
 0: titel
 1: voorstukje
 2: volledige tekst
+3: datum geschreven
 */
 
 let blogs = [
-["De eerste blog!", "Dit is de allereerste blog.", "Hoe vind je hem?"],
-["We hebben een cakerol gebakken!", "Vandaag hebben wij iets lekkers gemaakt. Weet jij al wat?", "Gekke geiten zijn overal, en vandaag hebben wij een lekkere cake gebakken. Het was echt super leuk, en we deden het met heel erg veel plezier."],
-["Geitenmelk...", "Ooit geitenmelk gedronken?", "Het is echt super lekker hoor!"],
-["Website leuk!", "De Website is u al een tijdje in de lucht, en dat vindt Team Kingston super fijn!", "Lees jij die blogs ook altijd? Ze gaan over van allemaal leuke dingen, maar een groot probleem is: ze zijn in het Nederlands! En dat vinden veel mensen natuurlijk niet leuk. Want iedereen wil ze natuurlijk in het Engels kunnen lezen..."],
+["De eerste blog!", "Dit is de allereerste blog.", "Hoe vind je hem?", "00-00-0000"],
+["We hebben een cakerol gebakken!", "Vandaag hebben wij iets lekkers gemaakt. Weet jij al wat?", "Gekke geiten zijn overal, en vandaag hebben wij een lekkere cake gebakken. Het was echt super leuk, en we deden het met heel erg veel plezier.", "00-00-0000"],
+["Geitenmelk...", "Ooit geitenmelk gedronken?", "Het is echt super lekker hoor!", "00-00-0000"],
+["Website leuk!", "De Website is u al een tijdje in de lucht, en dat vindt Team Kingston super fijn!", "Lees jij die blogs ook altijd? Ze gaan over van allemaal leuke dingen, maar een groot probleem is: ze zijn in het Nederlands! En dat vinden veel mensen natuurlijk niet leuk. Want iedereen wil ze natuurlijk in het Engels kunnen lezen...", "27-12-2021"],
 ];
 
 const holder = document.querySelector(".holder");
@@ -28,6 +29,11 @@ let titel = document.createElement("p");
 titel.setAttribute("class", "title");
 titel.textContent = blogs[a][0];
 top.appendChild(titel);
+
+let datumge = document.createElement("p");
+datumge.setAttribute("class", "datumge");
+datumge.textContent = blogs[a][3];
+top.appendChild(datumge);
 
 let voorinformatie = document.createElement("p");
 voorinformatie.setAttribute("class", "voorinformatie");
